51b67b794e06082064a3b209643e15239021be26
rainyhell13 <ekosychenko@gmail.com>
2026-06-29 03:56:30 +0300

    Исправление: транслитерация slug группы без расширения intl

--- app/models/Group.php
+++ app/models/Group.php
@@ -277,9 +277,16 @@ class Group extends Model
     private function makeSlug(string $name): string
     {
         $slug = mb_strtolower($name);
-        $slug = preg_replace('/[^a-zа-яёa-z0-9\s]/u', '', $slug);
+        // Транслитерация кириллицы в латиницу (без расширения intl)
+        $map = [
+            'а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d','е'=>'e','ё'=>'e','ж'=>'zh',
+            'з'=>'z','и'=>'i','й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n','о'=>'o',
+            'п'=>'p','р'=>'r','с'=>'s','т'=>'t','у'=>'u','ф'=>'f','х'=>'h','ц'=>'c',
+            'ч'=>'ch','ш'=>'sh','щ'=>'sch','ъ'=>'','ы'=>'y','ь'=>'','э'=>'e','ю'=>'yu','я'=>'ya',
+        ];
+        $slug = strtr($slug, $map);
         $slug = preg_replace('/\s+/', '_', trim($slug));
-        $slug = preg_replace('/[^a-z0-9_]/u', '', transliterator_transliterate('Any-Latin; Latin-ASCII; Lower()', $slug) ?? $slug);
+        $slug = preg_replace('/[^a-z0-9_]/', '', $slug);
         $slug = $slug ?: 'group';
         // Гарантируем уникальность
         $base = substr($slug, 0, 100);
